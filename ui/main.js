59db20bfa1051e09929b972a3737adacf70b1e33
ashmined <30647327+ashmined@users.noreply.github.com>
2017-08-08 21:01:38 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,5 @@
 console.log('Loaded!');
-var element = document.getElementById('main-text'
-);
-element.innerHTML = 'New Value';
+
 
 var img = document.getElementById('madi');
 img.onclick = function(){
