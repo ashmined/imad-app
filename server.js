dbad47130e20e2b4e9a07d438a8dcef0b89e7303
ashmined <30647327+ashmined@users.noreply.github.com>
2017-08-08 21:26:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -100,7 +100,7 @@ var counter = 0;
 app.get('/counter', function(req,res){
     counter = counter + 1;
     res.send(counter.toString());
-}
+});
 app.get('/article-two', function (req, res) {
     res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
